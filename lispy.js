318f0306069d9f96e0c55a71cc0c2dc650ae794e
Cillié Malan <cilliemalan@gmail.com>
2019-06-05 22:28:36 +0200

    Symbols!

--- lispy.js
+++ lispy.js
@@ -1,49 +1,86 @@
 
-const read = (text, offset = 0, len = 0) => {
+const read = (text) => {
+    const [r, _] = readInternal(text);
+    return r;
+}
+
+const readInternal = (text, offset = 0, len = 0) => {
     if (len == 0) len = text.length;
     if (len == 0) return undefined;
+
+    const rxSpace = /\s/;
+    const rxNumber = /[0-9.+-]/;
+    const rxSymbolStart = /[_$a-zA-Z\xA0-\uFFFF]/i;
+    const rxSymbolRest = /[_$a-zA-Z0-9\xA0-\uFFFF]/i;
+
+    let i = offset;
+    const result = (r) => [r, i - offset];
+    const space = (c) => /\s/.test(c) || c === "";
 
     let state = "form";
     let currentform = "";
 
-    for (let i = offset; i <= len; i++) {
-        const c = i < len ? text[i] : "eof";
+    for (; i <= len; i++) {
+        const c = i < len ? text[i] : "";
         switch (state) {
             case "form":
                 currentform = "";
-                if (c >= '0' && c <= '9' || c == '.' || c == '-' || c == '+') {
+                if (rxNumber.test(c)) {
                     state = "number";
                     currentform += c;
                     break;
                 }
-                else if (/\s/.test(c)) {
+                else if (rxSymbolStart.test(c)) {
+                    state = "symbol";
+                    currentform += c;
                     break;
                 }
-                else if (c == "") {
-                    return undefined;
+                else if (rxSpace.test(c)) {
+                    break;
+                }
+                else if (c === "") {
+                    return result(undefined);
                 }
                 else {
                     throw "unexpected character found: " + c;
                 }
+                break;
             case "number":
             case "number after decimal":
                 if (c >= '0' && c <= '9') {
                     currentform += c;
                     break;
                 }
-                else if (state == "number" && c == '.') {
+                else if (state === "number" && c === '.') {
                     currentform += c;
                     state = "number after decimal"
                     break;
                 }
-                else if (/\s/.test(c) || c == "eof") {
-                    return parseFloat(currentform);
+                else if (space(c)) {
+                    return result(parseFloat(currentform));
                 }
                 else {
-                    throw "unexpected character found: " + c;
+                    throw "unexpected character found while parsing number: " + c;
                 }
+                break;
+            case "symbol":
+                if (rxSymbolRest.test(c)) {
+                    currentform += c;
+                    break;
+                }
+                else if (space(c)) {
+                    return result(Symbol.for(currentform));
+                }
+                else {
+                    throw "unexpected character found while parsing symbol: " + c;
+                }
+                break;
+            default:
+                throw "unrecognized state: " + state;
         }
     }
+    
+    throw "exited loop without return";
 }
 
 
--- tests.js
+++ tests.js
@@ -10,6 +10,9 @@ const tests = {
     'read parses a negative simple number': () => assert.strictEqual(read("-123"), -123),
     'read parses a positive simple number': () => assert.strictEqual(read("+123"), +123),
     'read ignores leading spaces': () => assert.strictEqual(read("  123"), 123),
+
+    'read reads a symbol': () => assert.strictEqual(read("hello"), Symbol.for('hello')),
+    'read reads a symbol with leading spaces': () => assert.strictEqual(read("  helloA"), Symbol.for(' helloA')),
 }
 
 
@@ -20,6 +23,7 @@ Object.keys(tests).forEach(t => {
         process.stdout.write(`OK\n`);
     } catch (e) {
         process.stdout.write(`FAIL\n`);
-        console.error(e);
+        if(e.message) console.error(e.message);
+        else console.error(e);
     }
 });
