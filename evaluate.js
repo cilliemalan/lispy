79b5d9e7f02298fff2d29609f65ed50d6535cb40
Cillié Malan <cilliemalan@gmail.com>
2019-06-08 21:00:59 +0200

    Tweaked evaluate a bit more

--- evaluate.js
+++ evaluate.js
@@ -1,23 +1,26 @@
-const { isArray, isNumber, isBoolean, isString, isSymbol, isNull, isFunction } = require('util');
-const { Pair } = require('./reader');
+const { isArray, isNumber, isBoolean, isString, isSymbol, isFunction } = require('util');
+const { Macro } = require('./types');
 
-const isPair = (a) => a instanceof Pair;
-
-const evaluateLight = (expression, environment) => {
+const evaluate = (expression, environment) => {
     if (isNumber(expression)) return expression;
     else if (isBoolean(expression)) return expression;
     else if (isString(expression)) return expression;
-    else if (isPair(expression)) throw "Cannot evaluate a pair";
-    else if (isNull(expression)) throw "Cannot evaluate null";
+    else if (isSymbol(expression)) return environment(expression);
     else if (isArray(expression)) {
-        var func = evaluateLight(expression[0], environment);
-        if (!isFunction(func)) throw `cannot invoke non-function: ${func}`;
-        return func.apply(null, expression.slice(1).map(x => evaluateLight(x, environment)));
-    } else if (isSymbol(expression)) {
-        return environment(expression);
-    } else {
-        throw `unsupported type of expression: ${expression}`;
+        var func = evaluate(expression[0], environment);
+        if (isFunction(func)) {
+            return func.apply(null, expression.slice(1).map(x => evaluate(x, environment)));
+        }
+        else if (func instanceof Macro) {
+            return func.invoke.apply(null, expression.slice(1));
+        }
+        else {
+            throw `cannot invoke non-function: ${func}`;
+        }
+    }
+    else {
+        throw `cannot evaluate the expression: ${expression}`;
     }
 }
 
-module.exports = { evaluateLight };
+module.exports = { evaluate };
