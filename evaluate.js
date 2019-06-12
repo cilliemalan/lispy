9b977eea8b3ddb98280e529e813f05d9c4617e20
Cillié Malan <cilliemalan@gmail.com>
2019-06-12 11:43:08 +0200

    Refactored evaluate a bit

--- evaluate.js
+++ evaluate.js
@@ -1,25 +1,24 @@
 const { isArray, isNumber, isBoolean, isString, isSymbol, isFunction } = require('util');
 
-const evaluate = (expression, environment) => {
-    if (isNumber(expression)) return expression;
-    else if (isBoolean(expression)) return expression;
-    else if (isString(expression)) return expression;
-    else if (isSymbol(expression)) return environment(expression);
-    else if (isArray(expression)) {
-        const [rator, ...rand] = expression;
-        if (rator === Symbol.for('and')) {
+const s = (x) => Symbol.for(x);
+
+const evaluateInvocation = (expression, environment) => {
+    const [rator, ...rand] = expression;
+
+    switch (rator) {
+        case s('and'):
             if (rand.length == 0) return true;
             for (let i = 0; i < rand.length; i++) {
                 if (evaluate(rand[i], environment) === false) return false;
             }
             return true;
-        } else if (rator === Symbol.for('or')) {
+        case s('or'):
             if (rand.length == 0) return false;
             for (let i = 0; i < rand.length; i++) {
                 if (evaluate(rand[i], environment) !== false) return true;
             }
             return false;
-        } else if (rator === Symbol.for('if')) {
+        case s('if'):
             if (rand.length == 0) return false;
             const [c, t, e, ...nothing] = rand;
             if (nothing.length != 0) throw "if can have at most a then and an else clause";
@@ -27,9 +26,10 @@ const evaluate = (expression, environment) => {
                 return evaluate(t, environment);
             } else {
                 if (e === undefined) return undefined;
-                else evaluate(e, environment);
+                else return evaluate(e, environment);
             }
-        } else if (rator === Symbol.for('lambda')) {
+            break;
+        case s('lambda'):
             const [arguments, ...bodies] = rand;
             if (bodies.length === 0) throw "lambda must have at least one body";
             if (arguments.filter(a => !isSymbol(a)).length != 0) throw "arguments must be a list of symbols";
@@ -44,7 +44,8 @@ const evaluate = (expression, environment) => {
                 }
                 return result;
             }
-        } else {
+            break;
+        default:
             const func = evaluate(rator, environment);
 
             if (isFunction(func)) {
@@ -53,7 +54,16 @@ const evaluate = (expression, environment) => {
             } else {
                 throw `cannot invoke non-function: ${rator}`;
             }
-        }
+    }
+}
+
+const evaluate = (expression, environment) => {
+    if (isNumber(expression)) return expression;
+    else if (isBoolean(expression)) return expression;
+    else if (isString(expression)) return expression;
+    else if (isSymbol(expression)) return environment(expression);
+    else if (isArray(expression)) {
+        return evaluateInvocation(expression, environment);
     }
     else {
         throw `cannot evaluate the expression: ${expression}`;
